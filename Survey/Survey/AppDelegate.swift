18eb6eb61b5d148750f205a9f6af8024bb7fc861
Gavin Olsen <ols15015@byui.edu>
2017-04-13 10:30:11 -0600

    started project

---
+++ Survey/Survey/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Survey
+//
+//  Created by Gavin Olsen on 4/13/17.
+//  Copyright © 2017 Gavin Olsen. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
